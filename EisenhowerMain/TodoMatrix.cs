86a8c2ba1ed453cf0262c6e0908f40871cb669e9
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-09 11:50:00 +0100

    Add method ArchieveItems

--- EisenhowerMain/TodoMatrix.cs
+++ EisenhowerMain/TodoMatrix.cs
@@ -7,27 +7,36 @@ namespace EisenhowerCore
         Dictionary<string, TodoQuarter> todoQuarters = new Dictionary<string, TodoQuarter>();
         
 
-        public void AddItem(String title, DateTime deadline, bool isImportant){
-        //Add new item to map
-        DateTime today = DateTime.Now;
-        TimeSpan urgent = deadline - today;
-        if (urgent.Days <= 3){
-            if(isImportant){
-                todoQuarters["IU"].AddItem(title, deadline);
+        public void AddItem(String title, DateTime deadline, bool isImportant)
+        {
+            //Add new item to map
+            DateTime today = DateTime.Now;
+            TimeSpan urgent = deadline - today;
+            if (urgent.Days <= 3){
+                if(isImportant){
+                    todoQuarters["IU"].AddItem(title, deadline);
+                }
+                else{
+                    todoQuarters["NU"].AddItem(title, deadline);
+                }
             }
             else{
-                todoQuarters["NU"].AddItem(title, deadline);
+                if(isImportant){
+                    todoQuarters["IN"].AddItem(title, deadline);
+                }
+                else{
+                    todoQuarters["NN"].AddItem(title, deadline);
+                }
             }
         }
-        else{
-            if(isImportant){
-                todoQuarters["IN"].AddItem(title, deadline);
-            }
-            else{
-                todoQuarters["NN"].AddItem(title, deadline);
+
+        public void ArchiveItems()
+        {
+            //Removes all TodoItem object with parametr isDone from list todoItems
+            foreach (TodoQuarter quarter in todoQuarters.Values){
+                quarter.ArchiveItems();
             }
         }
-    }
         
     }
 
