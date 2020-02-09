8c17520bd15d2c233368d9dff108b01aa22d106f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-09 11:44:48 +0100

    Add AddItem method

--- EisenhowerMain/TodoMatrix.cs
+++ EisenhowerMain/TodoMatrix.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 namespace EisenhowerCore
 {
     public class TodoMatrix {
+        Dictionary<string, TodoQuarter> todoQuarters = new Dictionary<string, TodoQuarter>();
+        
 
+        public void AddItem(String title, DateTime deadline, bool isImportant){
+        //Add new item to map
+        DateTime today = DateTime.Now;
+        TimeSpan urgent = deadline - today;
+        if (urgent.Days <= 3){
+            if(isImportant){
+                todoQuarters["IU"].AddItem(title, deadline);
+            }
+            else{
+                todoQuarters["NU"].AddItem(title, deadline);
+            }
+        }
+        else{
+            if(isImportant){
+                todoQuarters["IN"].AddItem(title, deadline);
+            }
+            else{
+                todoQuarters["NN"].AddItem(title, deadline);
+            }
+        }
+    }
         
     }
 
